0f932b23133ac05f960570aa37ec6d513ab8dbf9
Andrew Haskell <andrew.haskell@ironwatch.net>
2026-04-08 07:53:17 +0000

    brightness in status bar using inotify instead of polling

---
+++ .config/custom/brightness.sh
@@ -0,0 +1,21 @@
+brightness_file='/sys/class/backlight/intel_backlight/brightness';
+max_brightness_file='/sys/class/backlight/intel_backlight/max_brightness';
+
+function printBrightness {
+  brightness=$(cat "$brightness_file");
+  max_brightness=$(cat "$max_brightness_file");
+  if [[ -z "$brightness" || -z "$max_brightness" || $max_brightness -eq 0 ]]; then
+    echo "󱓤";
+    return;
+  fi
+
+  percentage=$(( 100 * brightness / max_brightness ));
+
+  echo "<fn=1>󰖙</fn> <fc=slateblue>${percentage}</fc>%";
+}
+
+printBrightness;
+
+while inotifywait -qq -e modify "$brightness_file"; do
+  printBrightness;
+done
